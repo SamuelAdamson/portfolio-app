f8288ff346d658dbbfb7c6e1673f3652dde0f711
SamuelAdamson <sadamson.swe@gmail.com>
2025-01-12 19:52:29 -0500

    Adding socials and skeleton.css

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,10 +2,11 @@ import Alpine from 'alpinejs';
 
 // Components
 
-
 // Styles
+import './styles/normalize.css';
+import './styles/skeleton.css';
 import './styles/base.css';
-
+import './styles/components.css';
 
 window.Alpine = Alpine;
 Alpine.start();
